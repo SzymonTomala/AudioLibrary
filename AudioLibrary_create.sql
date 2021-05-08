0f5b33599328061fc145a5997ee3181d2a04e180
Adam Bednorz <248955@student.pwr.edu.pl>
2021-05-09 00:41:39 +0200

    add script to create database

---
+++ AudioLibrary_create.sql
@@ -0,0 +1,66 @@
+create table TypeOfAudio(
+id number generated always as identity,
+Type varchar(32) not null check(Type in('Song', 'AudioBook','Podcast')));
+
+create table AudioFile (
+id number generated always as identity,
+Title varchar(64) not null,
+AuthorId number not null,
+TypeOfAudioId number not null,
+Length int not null);
+
+create table Author (
+id number generated always as identity,
+FirstName varchar(64) not null,
+LastName varchar(64) not null);
+
+create table AudioUser(
+id number generated always as identity,
+FirstName varchar(64) not null,
+LastName varchar(64) not null,
+Email varchar(64) not null);
+
+create table Playlist(
+id number generated always as identity,
+LikeId number not null);
+
+create table Likes(
+id number generated always as identity,
+AudioUserId number not null,
+AudioFileId number not null);
+
+
+--###RELATIONS###
+    --###PRIMARY KEYS###
+alter table TypeOfAudio add constraint pk_TypeOfAudios
+primary key (id);
+alter table AudioFile add constraint pk_AudioFiles
+primary key (id);
+alter table Author add constraint pk_Authors
+primary key (id);
+alter table AudioUser add constraint pk_AudioUsers
+primary key (id);
+alter table Playlist add constraint pk_Playlists
+primary key (id);
+alter table Likes add constraint pk_Likes
+primary key (id);
+
+--###FOREIGN KEYS###
+alter table AudioFile add constraint fk_Author
+foreign key (AuthorId) references Author(id);
+
+alter table AudioFile add constraint fk_TypeOfAudio
+foreign key (TypeOfAudioId) references TypeOfAudio(id);
+
+alter table Playlist add constraint fk_Likes
+foreign key (LikeId) references Likes(id);
+
+alter table Likes add constraint fk_AudioUser
+foreign key (AudioUserId) references AudioUser(id);
+
+alter table Likes add constraint fk_AudioFile
+foreign key (AudioFileId) references AudioFile(id);
+
+
+
+
